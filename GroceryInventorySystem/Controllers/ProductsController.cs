623fe316c4226e11c08b271dc210eb2301e28cf8
knspromee <kararpromi7@gmail.com>
2026-07-28 01:59:43 +0600

    Added Products CRUD - List, Create, Edit, Delete

---
+++ GroceryInventorySystem/Controllers/ProductsController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GroceryInventorySystem.Data;
+using GroceryInventorySystem.Models;
+
+namespace GroceryInventorySystem.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Products (List all products)
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+
+        // GET: /Products/Create (Show form)
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Products/Create (Save product)
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.CreatedAt = DateTime.Now;
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: /Products/Edit/5 (Show edit form)
+        public IActionResult Edit(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: /Products/Edit/5 (Save changes)
+        [HttpPost]
+        public IActionResult Edit(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Products.Update(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // POST: /Products/Delete/5 (Delete product)
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
